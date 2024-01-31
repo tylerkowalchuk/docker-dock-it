6d5f7f6042ac39709cdbae826921f3ba7ef393a2
Tasheena Vigil <tasheena.vigil@gmail.com>
2024-01-30 20:41:39 -0600

    Implemented .env variables.

--- web/html/includes/database.php
+++ web/html/includes/database.php
@@ -1,8 +1,8 @@
 <?php
 $db = @mysqli_connect(
-	'db',
-	'example',
-	'example',
-	'world')
+	getenv("MYSQL_HOST"),
+	getenv("MYSQL_USER"),
+	getenv("MYSQL_PASSWORD"),
+	getenv("MYSQL_DATABASE"))
 		or die('Error connecting to database');
 		//or die('Error connecting to database: ' . mysqli_connect_error()); // for debugging
