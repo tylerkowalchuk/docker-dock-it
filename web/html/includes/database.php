4f3461288ad4e9ec6c4304f30ea91bb9d1f8c0d6
Megi Chkhetia <mchkhetia@my.wctc.edu>
2025-02-05 17:28:37 -0600

    changes made during Mondays class

--- web/html/includes/database.php
+++ web/html/includes/database.php
@@ -1,8 +1,9 @@
 <?php
 $db = @mysqli_connect(
-	'db',
-	'example',
-	'example',
-	'world')
+    getenv('MYSQL_HOST' ),
+	getenv('MSQL_USERNAME' ),
+	getenv('MSQL_PASSWORD'),
+    getenv('MYSQL_DATABASE')
+)
 		or die('Error connecting to database');
 		//or die('Error connecting to database: ' . mysqli_connect_error()); // for debugging
