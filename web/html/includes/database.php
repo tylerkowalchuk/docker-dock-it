7f39eaf577b8cbaca3b5dbae10a3ce842855ce49
Megi Chkhetia <mchkhetia@my.wctc.edu>
2025-02-08 17:41:15 -0600

    updated database.php

--- web/html/includes/database.php
+++ web/html/includes/database.php
@@ -1,5 +1,11 @@
 <?php
 
 // variable name = connect      server                  username            password                database
-$db = mysqli_connect( 'localhost',  'mchkhetia',  '000553239',  'adopt_pet')
+$db = mysqli_connect( 'db',  'mchkhetia',  '000553239',  'adopt_pet')
 or die('Unable to connect to the database');
+
+if (!$db) {
+    die("Connection failed: " . mysqli_connect_error());
+} else {
+    echo "Successfully connected to the database.";
+}
