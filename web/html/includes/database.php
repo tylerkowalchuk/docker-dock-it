d8c0b2ab6bf04de67efa413b4b3a1f57711871fc
Megi Chkhetia <mchkhetia@my.wctc.edu>
2025-02-08 17:23:09 -0600

    made changes to docker compose and database.php

--- web/html/includes/database.php
+++ web/html/includes/database.php
@@ -1,5 +1,5 @@
 <?php
 
 // variable name = connect      server                  username            password                database
-$db = mysqli_connect( 'localhost',  'mchkhetia',  '000553239',  'mchkhetia')
+$db = mysqli_connect( 'localhost',  'mchkhetia',  '000553239',  'adopt_pet')
 or die('Unable to connect to the database');
