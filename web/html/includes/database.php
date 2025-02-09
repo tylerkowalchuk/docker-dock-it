2d6551e6ebe5934934cd17a4e3c52b18c970e78b
mchkhetia <161772872+mchkhetia@users.noreply.github.com>
2025-02-08 19:19:23 -0600

    Update database.php

--- web/html/includes/database.php
+++ web/html/includes/database.php
@@ -1,6 +1,6 @@
 <?php
 
 // variable name = connect      server                  username            password                database
-$db = mysqli_connect( 'db',  'mchkhetia',  '000553239',  'adopt_pet')
+$db = mysqli_connect( 'db',  'mchkhetia',  'password',  'adopt_pet')
 or die('Unable to connect to the database');
 
